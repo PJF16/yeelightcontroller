09c9b62f49a67ece539fad9da233bd6d15db3748
Philipp Johann Fritz <mail@philipp.guru>
2017-11-27 17:47:18 +0100

    - added ability to specify power state (on/off)

--- src/yeelight/bulbs/controller/Controller.java
+++ src/yeelight/bulbs/controller/Controller.java
@@ -19,43 +19,72 @@ import java.util.logging.Logger;
  * @author Philipp
  */
 public class Controller {
-    
-    private final int BULB_PORT=55443;
+
+    private final int BULB_PORT = 55443;
     Socket clientSocket;
-    private static int id=1;
-    
-    
-    public void toggleBulb (String ip) {
-        try {
-            this.clientSocket = new Socket (ip,BULB_PORT);
-            DataOutputStream outdata = new DataOutputStream (clientSocket.getOutputStream());
-            String command = "{\"id\":"+id+",\"method\":\"toggle\",\"params\":[]}\r\n";
-            outdata.writeBytes(command);
-            id++;
-            clientSocket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+    private static int id = 1;
+
+    public void toggleBulb(ArrayList<String> myargs) {
+
+        for (int i = 0; i < myargs.size(); i++) {
+            try {
+                this.clientSocket = new Socket(myargs.get(i), BULB_PORT);
+                DataOutputStream outdata = new DataOutputStream(clientSocket.getOutputStream());
+                String command = "{\"id\":" + id + ",\"method\":\"toggle\",\"params\":[]}\r\n";
+                outdata.writeBytes(command);
+                id++;
+                clientSocket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
-    
-    public void startController (ArrayList<String> myargs) {
-        
+
+    private void setPower(ArrayList<String> myargs) {
+
+        if (myargs.size() >= 2) {
+            String state = myargs.get(0);
+            myargs.remove(0);
+            String duration = "500";
+
+            //now we should check if next object is an IP or a parameter; comes in the future
+            for (int i = 0; i < myargs.size(); i++) {
+                try {
+                    this.clientSocket = new Socket(myargs.get(i), BULB_PORT);
+                    DataOutputStream outdata = new DataOutputStream(clientSocket.getOutputStream());
+                    String command = "{\"id\":" + id + ",\"method\":\"set_power\",\"params\":[\""+state+"\",\"smooth\","+duration+"]}\r\n";
+                    outdata.writeBytes(command);
+                    id++;
+                    clientSocket.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            YeeLightBulbsController.printUsage();
+        }
+    }
+
+    public void startController(ArrayList<String> myargs) {
+
         String operation = myargs.get(0);
         myargs.remove(0);
-        
+
         this.executeOperation(operation, myargs);
     }
-    
-    private void executeOperation (String operation, ArrayList <String> myargs) {
-        
-        for (int i=0; i<myargs.size();i++) {
-            switch (operation) {
-                case "t":
-                    this.toggleBulb(myargs.get(i));
-                    break;
-                default:
-                    YeeLightBulbsController.printUsage();
-            }
+
+    private void executeOperation(String operation, ArrayList<String> myargs) {
+
+        switch (operation) {
+            case "t":
+                this.toggleBulb(myargs);
+                break;
+            case "power":
+            case "p":
+                this.setPower(myargs);
+                break;
+            default:
+                YeeLightBulbsController.printUsage();
         }
     }
 }
